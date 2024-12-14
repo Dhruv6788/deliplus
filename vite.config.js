47d6c56f02e97cb10812cee2481eac5fc0a0adb0
Dhruv Joshi <jdhruv036@gmail.com>
2024-12-14 19:18:28 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+
+
+export default {
+    root: '/',
+    publicDir: './src/assets',
+    base: './',
+    server:
+    {
+        host: true, // Open to local network and display URL
+    },
+    build:
+    {
+        outDir: './dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+    plugins:[]
+}
