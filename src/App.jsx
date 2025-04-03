41fb68515a595a8c9362f737bfee20706f2fe576
Dhruv Joshi <jdhruv036@gmail.com>
2025-04-03 09:45:30 +0530

    Menu Refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routesConfig } from "./app/routes/routes";
 import "./App.css";
 
+const router = createBrowserRouter(routesConfig); // Create router outside the component
+
 function App() {
   return (
     <div className="app">
-        <RouterProvider router={createBrowserRouter(routesConfig)} />
-        
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -1,42 +1,29 @@
-import HomeLayout from "../../components/layouts/HomeLayout/HomeLayout";
-import Home from "../../components/pages/home/Home";
-import Menu from  '../../components/pages/menu/Menu'
-import About from '../../components/pages/about/About'
-import ErrorBoundary from '../../components/common/ErrorBoundry'
-import ReceipeList from "../../components/common/recepiList";
-import { recipeList } from "../../constants/dumpData";
-import { Navigate } from "react-router-dom";
 import Contact from "../../components/pages/contact/Contact";
 
 export const routesConfig = [
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     errorElement: <ErrorBoundary />,
     children: [
       {
-        index: true, // Makes this the default child route
+        index: true, 
         element: <Home />
       },
       {
-        path : '/menu', 
-        element : <Menu /> ,
-        children : [
-          {
-            index : true,
-            element : <Menu />
-          }
-        ],
-        errorElement : <ErrorBoundary />
+        path: "menu",  // No leading slash
+        element: <Menu />,
+        errorElement: <ErrorBoundary />
       },
       {
-        path : '/about', 
-        element : <About />
+        path: "about", 
+        element: <About />
       }, 
       {
-        path : 'contact',
-        element : <Contact />
+        path: "contact", // Removed leading "/"
+        element: <Contact />
       }
     ]
   },
-]
+];
+ 
